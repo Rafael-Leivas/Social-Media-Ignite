04351be28454100427f83defe1c34788106a9f25
Rafael Bortnik Leivas <rafaelBLSteam@gmail.com>
2023-03-30 16:11:09 -0300

    project social media do curso Ignait da Roketseat

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author='Rafael Leivas'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iure reprehenderit explicabo aperiam maiores asperiores eligendi autem commodi eaque veritatis accusantium optio, labore aliquid aut quam assumenda. Fugit, libero quam!'
+      />
+      <Post 
+        author='Felipe Lipe'
+        content='Matos gay'
+      />
+    </div>
+    )
+}
